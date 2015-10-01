035fb0e7441153a6381286c89661838a9a2a2ad5
quan <quanchen@umich.edu>
2015-09-30 22:37:12 -0400

    load gen

--- src/edu/umich/clarity/loadGen.java
+++ src/edu/umich/clarity/loadGen.java
@@ -18,6 +18,51 @@ public class loadGen {
     private static double poisson_bg_mean=200;
     private static String POISSON_TARGET_SAMPLE_FILE="input/target_load.csv";
     private static String POISSON_BG_SAMPLE_FILE="input/bg_load.csv";
+
+    private static double poisson_dig_target_mean=300;
+    private static double poisson_dig_bg_mean=140;
+    private static String POISSON_DIG_TARGET_SAMPLE_FILE="input/dig_target_load.csv";
+    private static String POISSON_DIG_BG_SAMPLE_FILE="input/dig_bg_load.csv";
+
+    private static double poisson_face_target_mean=600;
+    private static double poisson_face_bg_mean=300;
+    private static String POISSON_FACE_TARGET_SAMPLE_FILE="input/face_target_load.csv";
+    private static String POISSON_FACE_BG_SAMPLE_FILE="input/face_bg_load.csv";
+
+    private static double poisson_imc_target_mean=400;
+    private static double poisson_imc_bg_mean=250;
+    private static String POISSON_IMC_TARGET_SAMPLE_FILE="input/imc_target_load.csv";
+    private static String POISSON_IMC_BG_SAMPLE_FILE="input/imc_bg_load.csv";
+    
+    private static double poisson_ner_target_mean=50;
+    private static double poisson_ner_bg_mean=10;
+    private static String POISSON_NER_TARGET_SAMPLE_FILE="input/ner_target_load.csv";
+    private static String POISSON_NER_BG_SAMPLE_FILE="input/ner_bg_load.csv";
+    
+    private static double poisson_pos_target_mean=50;
+    private static double poisson_pos_bg_mean=10;
+    private static String POISSON_POS_TARGET_SAMPLE_FILE="input/pos_target_load.csv";
+    private static String POISSON_POS_BG_SAMPLE_FILE="input/pos_bg_load.csv";
+
+    private static double poisson_chk_target_mean=50;
+    private static double poisson_chk_bg_mean=10;
+    private static String POISSON_CHK_TARGET_SAMPLE_FILE="input/chk_target_load.csv";
+    private static String POISSON_CHK_BG_SAMPLE_FILE="input/chk_bg_load.csv";
+
+    private static double poisson_stemmer_target_mean=410;
+    private static double poisson_stemmer_bg_mean=150;
+    private static String POISSON_STEMMER_TARGET_SAMPLE_FILE="input/stemmer_target_load.csv";
+    private static String POISSON_STEMMER_BG_SAMPLE_FILE="input/stemmer_bg_load.csv";
+    
+    private static double poisson_asr_target_mean=410;
+    private static double poisson_asr_bg_mean=200;
+    private static String POISSON_ASR_TARGET_SAMPLE_FILE="input/asr_target_load.csv";
+    private static String POISSON_ASR_BG_SAMPLE_FILE="input/asr_bg_load.csv";
+    
+    private static double poisson_gmm_target_mean=110;
+    private static double poisson_gmm_bg_mean=50;
+    private static String POISSON_GMM_TARGET_SAMPLE_FILE="input/gmm_target_load.csv";
+    private static String POISSON_GMM_BG_SAMPLE_FILE="input/gmm_bg_load.csv";
     
     // for burst load
 //    private static double burst_high_mean;
@@ -42,6 +87,34 @@ public class loadGen {
         loadGen client = new loadGen();
         client.genPoissonSamples(poisson_target_mean, 10000, POISSON_TARGET_SAMPLE_FILE);
         client.genPoissonSamples(poisson_bg_mean, 10000, POISSON_BG_SAMPLE_FILE);
+        
+        client.genPoissonSamples(poisson_dig_target_mean, 10000, POISSON_DIG_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_dig_bg_mean, 10000, POISSON_DIG_BG_SAMPLE_FILE);
+
+        client.genPoissonSamples(poisson_face_target_mean, 10000, POISSON_FACE_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_face_bg_mean, 10000, POISSON_FACE_BG_SAMPLE_FILE);
+
+        client.genPoissonSamples(poisson_imc_target_mean, 10000, POISSON_IMC_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_imc_bg_mean, 10000, POISSON_IMC_BG_SAMPLE_FILE);
+
+        client.genPoissonSamples(poisson_ner_target_mean, 10000, POISSON_NER_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_ner_bg_mean, 10000, POISSON_NER_BG_SAMPLE_FILE);
+        
+        client.genPoissonSamples(poisson_pos_target_mean, 10000, POISSON_POS_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_pos_bg_mean, 10000, POISSON_POS_BG_SAMPLE_FILE);
+        
+        client.genPoissonSamples(poisson_chk_target_mean, 10000, POISSON_CHK_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_chk_bg_mean, 10000, POISSON_CHK_BG_SAMPLE_FILE);
+        
+        client.genPoissonSamples(poisson_stemmer_target_mean, 10000, POISSON_STEMMER_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_stemmer_bg_mean, 10000, POISSON_STEMMER_BG_SAMPLE_FILE);
+        
+        client.genPoissonSamples(poisson_asr_target_mean, 10000, POISSON_ASR_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_asr_bg_mean, 10000, POISSON_ASR_BG_SAMPLE_FILE);
+        
+        client.genPoissonSamples(poisson_gmm_target_mean, 10000, POISSON_GMM_TARGET_SAMPLE_FILE);
+        client.genPoissonSamples(poisson_gmm_bg_mean, 10000, POISSON_GMM_BG_SAMPLE_FILE);
+        
 //        client.genBurstSamples(100, 200, 10000);
         LOG.info("Load generation completes");
     }
@@ -54,7 +127,8 @@ public class loadGen {
 
             ArrayList<String> csvEntry = new ArrayList<String>();
             for (int i = 0; i < sampleNum; i++) {
-                csvEntry.add(poi_dist.sample() + "");
+//                csvEntry.add(poi_dist.sample() + "");
+                csvEntry.add((int)(mean) + "");
             }
             sampleWriter.writeNext(csvEntry.toArray(new String[csvEntry.size()]));
             sampleWriter.flush();
